c1cd4df68a32bf60840d6317226e45fd158cce40
Maciej Strach <maciej.strach01@gmail.com>
2024-05-04 21:28:36 +0200

    php authorization

--- index.php
+++ index.php
@@ -5,7 +5,10 @@ require 'Routing.php';
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
+// Routing::get('','DefaultController');
 Routing::get('index','DefaultController');
 Routing::get('projects','DefaultController');
+// Routing::get('projects','DefaultController');
+Routing::post('login','SecurityController');
 
 Routing::run($path);
---
+++ src/models/User.php
@@ -0,0 +1,57 @@
+<?php
+
+class User
+{
+    private $email;
+    private $password;
+    private $name;
+    private $surname;
+
+    public function __construct(string $email, string $password, string $name, string $surname)
+    {
+        $this->email = $email;
+        $this->password = $password;
+        $this->name = $name;
+        $this->surname = $surname;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setSurname(string $surname)
+    {
+        $this->surname = $surname;
+    }
+
+    public function getSurname(): string
+    {
+        return $this->surname;
+    }
+}
